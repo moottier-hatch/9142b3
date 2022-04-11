a92fd1d3396e355df88a5defcc4ee3423927a583
Andre Mottier <andre.mottier@gmail.com>
2022-04-10 20:22:57 -0700

    update db for multi-user chat

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -4,25 +4,27 @@ from django.db.models import Q
 from . import utils
 from .user import User
 
+class UserConversations(utils.CustomModel):
+    """conversations for each user
+    """
+    userId = models.ForeignKey(
+        "User", on_delete=models.CASCADE, related_name="+"
+    )
+    conversationId = models.ForeignKey(
+        "Conversation", on_delete=models.CASCADE, related_name="conversations"
+    )
+    
+    class Meta:
+        unique_together = ('userId', 'conversationId',)
 
 class Conversation(utils.CustomModel):
-
-    user1 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user1Id", related_name="+"
-    )
-    user2 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user2Id", related_name="+", 
-    )
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
-    # find conversation given two user Ids
-    def find_conversation(user1Id, user2Id):
-        # return conversation or None if it doesn't exist
+    # find a set of conversation given an iterable containing user Ids
+    def find_conversation(users):
+        # return a set of conversation or None if it doesn't exist
         try:
-            return Conversation.objects.get(
-                (Q(user1__id=user1Id) | Q(user1__id=user2Id)),
-                (Q(user2__id=user1Id) | Q(user2__id=user2Id)),
-            )
+            return Conversation.objects.filter(UserConversations__pk__in=users)
         except Conversation.DoesNotExist:
             return None
