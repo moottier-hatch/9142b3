422f5dacdf3f3f7c7592abadfd903d97e1c542e6
Andre Mottier <andre.mottier@gmail.com>
2022-04-11 19:15:55 -0700

    add UserConversations model

---
+++ server/messenger_backend/models/conversation.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.db.models import Q
+
+from . import utils
+from .user import User
+
+class UserConversations(utils.CustomModel):
+    """conversations for each user
+    """
+    userId = models.ForeignKey(
+        "User", on_delete=models.CASCADE, related_name="+"
+    )
+    conversationId = models.ForeignKey(
+        "Conversation", on_delete=models.CASCADE, related_name="conversations"
+    )
+    
+    class Meta:
+        unique_together = ('userId', 'conversationId',)
+
+class Conversation(utils.CustomModel):
+    user1 = models.ForeignKey(User, db_column='user1Id', on_delete=models.CASCADE, related_name='+')
+    user2 = models.ForeignKey(User, db_column='user2Id', on_delete=models.CASCADE, related_name='+')
+    createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    # find a set of conversation given an iterable containing user Ids
+    def find_conversation(users):
+        # return a set of conversation or None if it doesn't exist
+        try:
+            return Conversation.objects.filter(UserConversations__pk__in=users)
+        except Conversation.DoesNotExist:
+            return None
